a2cc6dfa7968a3c0f18e1dd1de6ee379fafc6aba
Kurillccc <89049125360@mail.ru>
2025-01-17 09:11:31 +0300

    Initial Commit

---
+++ ShoppingList2/ShoppingList2App.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingList2App.swift
+//  ShoppingList2
+//
+//  Created by Кирилл on 17.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingList2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShoppingList2Tests/ShoppingList2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingList2Tests.swift
+//  ShoppingList2Tests
+//
+//  Created by Кирилл on 17.01.2025.
+//
+
+import Testing
+@testable import ShoppingList2
+
+struct ShoppingList2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
